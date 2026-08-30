7cad0a38127f78a4b81aa8baba0b4b39a294d1a5
Lussssya <lusya_sargsyan@edu.aua.am>
2026-08-30 19:14:01 +0400

    fix

--- src/main/java/io/github/lussssya/residentialparking/parking/api/rest/ParkingSpotResponse.java
+++ src/main/java/io/github/lussssya/residentialparking/parking/api/rest/ParkingSpotResponse.java
@@ -2,15 +2,14 @@ package io.github.lussssya.residentialparking.parking.api.rest;
 
 import io.github.lussssya.residentialparking.parking.domain.model.ParkingSpot;
 import io.github.lussssya.residentialparking.parking.domain.model.ParkingSpotStatus;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public record ParkingSpotResponse(
-        @NotNull UUID id,
-        @NotNull UUID communityId,
-        @NotNull String code,
-        @NotNull ParkingSpotStatus status
+        UUID id,
+        UUID communityId,
+        String code,
+        ParkingSpotStatus status
 ) {
     public static ParkingSpotResponse from (ParkingSpot parkingSpot) {
         return new ParkingSpotResponse(
